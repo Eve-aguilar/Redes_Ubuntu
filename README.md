52aeb50eefe8a9252eef7ed0766eaf3215a300e4
DavAguilarVargas <davidaguilarvargas963@gmail.com>
2023-08-17 10:15:35 -0400

    Sistema XD

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# Redes_Ubuntu
+# Tutorial CRUD de neoguias.com
+
+[Tutorial CRUD PHP en neoguias](https://www.neoguias.com/tutorial-crud-php/)
---
+++ config.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+  'db' => [
+    'host' => '192.168.216.130',
+    'user' => 'root',
+    'pass' => 'Admin2023._3v3lyn_123',
+    'name' => 'tutorial_crud',
+    'options' => [
+      PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+    ]
+  ]
+];
